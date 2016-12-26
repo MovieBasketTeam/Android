859dc414cd797fe041834d15592bc12cdfa147f6
seomoon <tjans1457@naver.com>
2016-12-26 18:40:19 +0900

    seomoon splash

--- Client/app/src/main/java/com/moviebasket/android/client/splash/SplashActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/splash/SplashActivity.java
@@ -1,16 +1,34 @@
 package com.moviebasket.android.client.splash;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import com.moviebasket.android.client.R;
+import com.moviebasket.android.client.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
+
+
+
+
 
 
     }
