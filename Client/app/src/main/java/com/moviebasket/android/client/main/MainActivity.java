f993dfc45df735f61755147b13feb68c0838c150
forgreen0624 <forgreen0624@naver.com>
2016-12-26 22:50:11 +0900

    main화면에 drawer 추가(계획), 리사이클러뷰 추가

--- Client/app/src/main/java/com/moviebasket/android/client/main/MainActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/main/MainActivity.java
@@ -2,14 +2,40 @@ package com.moviebasket.android.client.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.moviebasket.android.client.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = (RecyclerView)findViewById(R.id.rv_main);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_button_drawer) {
+            //drawer 오픈. 추후
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_actionbar_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
