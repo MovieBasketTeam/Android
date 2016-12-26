f56c5e07d138ae88ee1ca78f6da78919195c9c3e
wlldnjs <wlldnjs@naver.com>
2016-12-26 20:49:45 +0900

    jiwon - style(noActionbar), splash, join, login 추가

--- Client/app/src/main/java/com/moviebasket/android/client/splash/SplashActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/splash/SplashActivity.java
@@ -1,18 +1,28 @@
 package com.moviebasket.android.client.splash;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.moviebasket.android.client.R;
+import com.moviebasket.android.client.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
+    //스플래시 화면 추가
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Handler hd = new Handler(){
+            public void handleMessage(Message msg){
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
 
-        //
-
+            }
+        };
+        hd.sendEmptyMessageDelayed(0, 3000);
     }
 }
