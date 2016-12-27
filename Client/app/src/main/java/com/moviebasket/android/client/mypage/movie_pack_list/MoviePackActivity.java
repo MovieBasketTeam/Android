e6cf4984a5607b1d7d0e46031c3b7aff9187e65c
Leepilju <di002@naver.com>
2016-12-27 19:36:28 +0900

    pilju base

--- Client/app/src/main/java/com/moviebasket/android/client/mypage/movie_pack_list/MoviePackActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/mypage/movie_pack_list/MoviePackActivity.java
@@ -11,5 +11,6 @@ public class MoviePackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_pack);
+
     }
 }
