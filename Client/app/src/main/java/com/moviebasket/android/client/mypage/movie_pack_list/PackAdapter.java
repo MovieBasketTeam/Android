ebcfdb43f7e486c3ae5b1c2ec31c5bb5e6d7bbcf
Leepilju <di002@naver.com>
2016-12-28 00:46:59 +0900

    pilju

---
+++ Client/app/src/main/java/com/moviebasket/android/client/mypage/movie_pack_list/PackAdapter.java
@@ -0,0 +1,57 @@
+package com.moviebasket.android.client.mypage.movie_pack_list;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import com.moviebasket.android.client.R;
+
+
+/**
+ * Created by pilju on 2016-12-27.
+ */
+
+
+public class PackAdapter extends RecyclerView.Adapter<PackViewHolder> {
+
+    ArrayList<PackDatas> mDatas;
+
+    public PackAdapter() {
+
+    }
+
+    public PackAdapter(ArrayList<PackDatas> mDatas) {
+
+        this.mDatas = mDatas;
+    }
+
+
+    @Override
+    public PackViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 뷰홀더 패턴을 생성하는 메소드.
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_pack, parent, false);
+        PackViewHolder viewHolder = new PackViewHolder(itemView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PackViewHolder holder, int position) {
+        //리싸이클뷰에 항목을 뿌려주는 메소드.
+        holder.movie_image.setImageResource(mDatas.get(position).image);
+        holder.owner.setText(mDatas.get(position).owner);
+        holder.likecount.setText(mDatas.get(position).likecount);
+        holder.title.setText(mDatas.get(position).title);
+        holder.direct_country.setText(mDatas.get(position).directer + "/" +mDatas.get(position).country);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return (mDatas != null) ? mDatas.size() : 0;
+    }
+}
+
