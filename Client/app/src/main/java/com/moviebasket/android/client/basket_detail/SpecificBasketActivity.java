d1dc62ccdee2ab1d895c48161f09c15243a64ecd
forgreen0624 <forgreen0624@naver.com>
2016-12-28 16:58:22 +0900

    activity 구조, package 구조 정리

---
+++ Client/app/src/main/java/com/moviebasket/android/client/basket_detail/SpecificBasketActivity.java
@@ -0,0 +1,15 @@
+package com.moviebasket.android.client.basket_detail;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.moviebasket.android.client.R;
+
+public class SpecificBasketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specific_basket);
+    }
+}
