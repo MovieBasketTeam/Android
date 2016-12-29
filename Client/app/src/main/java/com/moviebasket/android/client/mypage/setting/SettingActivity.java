2c14309976ba0914fe5f865aa24f8317c161b2a4
seomoon <tjans1457@naver.com>
2016-12-30 04:24:18 +0900

    muny setting_activity_1

--- Client/app/src/main/java/com/moviebasket/android/client/mypage/setting/SettingActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/mypage/setting/SettingActivity.java
@@ -1,31 +1,44 @@
 package com.moviebasket.android.client.mypage.setting;
 
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.moviebasket.android.client.R;
+import com.moviebasket.android.client.main.MainActivity;
 
 public class SettingActivity extends AppCompatActivity {
+
+    ImageView backbutton;
+    ImageView userimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        // ActionBar에 타이틀 변경
-        getSupportActionBar().setTitle("환경설정");
-        // ActionBar의 배경색 변경
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF663300));
-
-        // 홈 아이콘 표시
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-
-
-
-
+        backbutton = (ImageView) findViewById(R.id.backbutton);
+        userimage = (ImageView) findViewById(R.id. userimage1);
+        backbutton.setOnClickListener(clickListener);
+        userimage.setOnClickListener(clickListener);
 
     }
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+               case R.id.backbutton:
+                   Intent BackIntent = new Intent(SettingActivity.this, MainActivity.class);
+                   startActivity(BackIntent);
+                   finish();
+                   break;
+                case R.id.userimage1:
+                    Toast.makeText(SettingActivity.this, "사진 바꾸기 준비중", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 }
