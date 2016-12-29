3f39e1dcfa2f0bc8d70cab25778d5a647e303682
forgreen0624 <forgreen0624@naver.com>
2016-12-29 20:43:27 +0900

    다시 돌림

--- Client/app/src/main/java/com/moviebasket/android/client/join/JoinActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/join/JoinActivity.java
@@ -7,9 +7,12 @@ import com.moviebasket.android.client.R;
 
 public class JoinActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+
     }
 }
