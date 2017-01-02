ce7bb3c1b2affd49a4f65e3de0f73c38a733976b
seomoon <tjans1457@naver.com>
2017-01-03 07:25:56 +0900

    seomoon 담은영화 하트 제거 했지롱 서문이가

--- Client/app/src/main/java/com/moviebasket/android/client/security/SecurityDataSet.java
+++ Client/app/src/main/java/com/moviebasket/android/client/security/SecurityDataSet.java
@@ -6,10 +6,5 @@ package com.moviebasket.android.client.security;
 
 public final class SecurityDataSet {
     //이거는 절대 푸시하면 안됨!!!!!!!!!!!!!!!!!!!!!!!!
-    public static final String MBServerUrl = "http://52.78.217.168:3000";
-    public static final String NaverServiceClientID = "WhcQHOAcgYBIfWgQmNJt";
-    public static final String NaverServiceClientSecret = "rowki2RPNW";
-    public static final String STR_NAME = "MovieBasketStore";
-    public static final String TK_KEY = "MBT";
 
 }
