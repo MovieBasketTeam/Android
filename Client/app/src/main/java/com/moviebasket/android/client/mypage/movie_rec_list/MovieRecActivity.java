d6aba1353c679ba76d55081ec2f14e67d834fadc
Leepilju <di002@naver.com>
2017-01-03 05:41:26 +0900

    추천영화 detail 추가 - PILJU

--- Client/app/src/main/java/com/moviebasket/android/client/mypage/movie_rec_list/MovieRecActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/mypage/movie_rec_list/MovieRecActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.moviebasket.android.client.R;
 import com.moviebasket.android.client.clickable.OneClickable;
 import com.moviebasket.android.client.global.ApplicationController;
+import com.moviebasket.android.client.movie_detail.MovieDetailDialog;
 import com.moviebasket.android.client.network.MBService;
 
 import java.util.ArrayList;
@@ -32,6 +33,7 @@ public class MovieRecActivity extends AppCompatActivity implements OneClickable 
     private boolean isResponseSuccess;
     private boolean isHeartSuccess;
     private static final String FAILURE = "view failed";
+    private MovieDetailDialog detailDialog;
     int position;
 
     LinearLayoutManager mLayoutManager;
@@ -108,24 +110,14 @@ public class MovieRecActivity extends AppCompatActivity implements OneClickable 
             //2.position번째 항목의 Data를 가져오는 방법
             String MovieTitle = mDatas.get(position).movie_title;
 
-            //3.여기서부터는 각자 알아서 처리해야할 것을 코딩해야함.
-            //ex) 충민: 바스켓 리스트를 누르면 그 항목의 바스켓 상세페이지로 이동시켜야함.
-            //Intent BasketDetailIntent = new Intent(MainActivity.this, )
-            //Toast.makeText(MovieSearchActivity.this, position+"번째 리사이클러뷰 항목 클릭!", Toast.LENGTH_SHORT).show();
-            Toast.makeText(MovieRecActivity.this, MovieTitle, Toast.LENGTH_SHORT).show();
-
+            //영화 상세보기 다이얼로그를 띄워주기 위함
+            detailDialog = new MovieDetailDialog(MovieRecActivity.this);
+            detailDialog.show();
         }
     };
 
     @Override
     public void processOneMethodAtPosition(final int position) {
-
-        Toast.makeText(MovieRecActivity.this, "하트를 눌렀당", Toast.LENGTH_SHORT).show();
-//                    position = recyclerView
-//                    position = recyclerView.getChildLayoutPosition(v.getParent());
-        // heart.setOnClickListener(recylerClickListener);
-
-
         Call<HeartResult> getHeartReasult = mbService.getHeartResult(mDatas.get(position).movie_id, mDatas.get(position).is_liked, token);
         getHeartReasult.enqueue(new Callback<HeartResult>() {
             @Override
@@ -141,19 +133,7 @@ public class MovieRecActivity extends AppCompatActivity implements OneClickable 
                     Log.i("Heart", "무비아이디 : " + mDatas.get(position).movie_id);
                 }
                 if (isHeartSuccess) {
-                    //mDatas.addAll(heartResult.result.Message);
-//                    Log.i("isHeartSuccess", "들어왔다" + isHeartSuccess);
-//                    Log.i("확인", "확인" + mDatas.get(position).movie_id + mDatas.get(position).is_liked);
-//                    if (mDatas.get(position).is_liked == 1) {
-//                        mDatas.get(position).is_liked = 0;
-//                        mDatas.get(position).movie_like--;
-//                    } else {
-//                        mDatas.get(position).is_liked = 1;
-//                        mDatas.get(position).movie_like++;
-//                    }
-
                     mDatas.remove(position);
-
                     adapter.notifyDataSetChanged();
                 }
             }
@@ -168,65 +148,3 @@ public class MovieRecActivity extends AppCompatActivity implements OneClickable 
 
     }
 }
-
-//    View.OnClickListener clickListener = new View.OnClickListener() {
-//
-//        @Override
-//        public void onClick(View v) {
-//            position=recyclerView.getChildLayoutPosition(v);
-//
-//            switch (v.getId()) {
-//                case R.id.book_mark:
-//
-//                    Toast.makeText(MovieRecActivity.this, "담기를 눌렸당 이건 지울것", Toast.LENGTH_SHORT).show();
-//                    break;
-//                case R.id.heart:
-//                    Toast.makeText(MovieRecActivity.this, "하트를 눌렀당", Toast.LENGTH_SHORT).show();
-////                    position = recyclerView
-////                    position = recyclerView.getChildLayoutPosition(v.getParent());
-//                    // heart.setOnClickListener(recylerClickListener);
-//
-//
-//                    Call<HeartResult> getHeartReasult = mbService.getHeartResult(mDatas.get(position).movie_id, mDatas.get(position).is_liked, token);
-//                    getHeartReasult.enqueue(new Callback<HeartResult>() {
-//                        @Override
-//                        public void onResponse(Call<HeartResult> call, Response<HeartResult> response) {
-//                            Log.i("NetConfirm", "onResponse: 하트에들어옴");
-//                            HeartResult heartResult = response.body();
-//                            if (response.isSuccessful()) {// 응답코드 200
-//                                Log.i("Heart", "요청메시지:" + call.toString() + " 응답메시지:" + response.toString());
-//                                Log.i("Heart", "응답 결과 : " + heartResult.result.message);
-//                                isHeartSuccess = heartResult.result.message != null ? true : false;
-//                                Log.i("Heart", "응답 결과 : " + isHeartSuccess);
-//                                Log.i("Heart", "포지션 : " + mDatas.get(position));
-//                                Log.i("Heart", "무비아이디 : " + mDatas.get(position).movie_id);
-//                            }
-//                            if (isHeartSuccess) {
-//                                //mDatas.addAll(heartResult.result.Message);
-//                                Log.i("isHeartSuccess", "들어왔다" + isHeartSuccess);
-//                                Log.i("확인", "확인" + mDatas.get(position).movie_id + mDatas.get(position).is_liked);
-//                                if (mDatas.get(position).is_liked == 1) {
-//                                    mDatas.get(position).is_liked = 0;
-//                                    mDatas.get(position).movie_like--;
-//                                }else{
-//                                    mDatas.get(position).is_liked = 1;
-//                                    mDatas.get(position).movie_like++;
-//                                }
-//
-//                                adapter.notifyDataSetChanged();
-//                            }
-//                        }
-//
-//                        @Override
-//                        public void onFailure(Call<HeartResult> call, Throwable t) {
-//                            Log.i("NetConfirm", "onFailure: 들어옴" + call.toString());
-//                            Toast.makeText(MovieRecActivity.this, "서비스에 오류가 있습니다.", Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//
-//                    break;
-//
-//            }
-//        }
-//    };
-//}
