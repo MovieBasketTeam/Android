80acd548b6dd5c8079847bbdd0d5f1ead08188da
forgreen0624 <forgreen0624@naver.com>
2017-01-06 02:54:09 +0900

    홈화면 이동하는거 추가하기 진행중~

--- Client/app/src/main/java/com/moviebasket/android/client/basket_detail/SpecificBasketActivity.java
+++ Client/app/src/main/java/com/moviebasket/android/client/basket_detail/SpecificBasketActivity.java
@@ -2,6 +2,7 @@ package com.moviebasket.android.client.basket_detail;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,6 +17,7 @@ import com.bumptech.glide.Glide;
 import com.moviebasket.android.client.R;
 import com.moviebasket.android.client.clickable.TwoClickable;
 import com.moviebasket.android.client.global.ApplicationController;
+import com.moviebasket.android.client.main.MainActivity;
 import com.moviebasket.android.client.movie_detail.MovieDetailDialog;
 import com.moviebasket.android.client.mypage.basket_list.BasketResult;
 import com.moviebasket.android.client.mypage.movie_rec_list.HeartResult;
@@ -75,6 +77,11 @@ public class SpecificBasketActivity extends AppCompatActivity implements TwoClic
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "홈", Toast.LENGTH_SHORT).show();
+                //홈화면으로 가는거
+                Intent homeIntent = new Intent(SpecificBasketActivity.this, MainActivity.class);
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK  | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(homeIntent);
+                finish();
             }
         });
 
